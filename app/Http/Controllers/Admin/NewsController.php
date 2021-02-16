ac463f64eeecf7e145e3fbbe6fc8d2c3d9a366aa
HinataOe <hinatan430_6521@yahoo.co.jp>
2021-02-16 03:28:02 +0000

    text14 done

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\News;
+
 class NewsController extends Controller
 //自分が実装したい機能をここに入れていく。
 {
@@ -15,6 +17,29 @@ class NewsController extends Controller
     
     public function create(Request $request)
     {
+        //Validationを行う。
+        $this->validate($request, News::$rules);//News Modelの$rulesメソッドにアクセス
+        
+        $news = new News;
+        $form = $request->all();
+        
+        //フォームから画像が送信されてきたら、保存して、$news->image_pathに画像のパスを保存する。
+        if(isset($form['image'])) {
+            $path =$request->file('image')->store('public/image');
+            $news ->image_path = basename($path);
+        }else{
+            $news->image_path = null;
+        }
+        
+        //フォームから送信されてきた_tokenを削除するコード
+        unset($form['token']);
+        //フォームから送信されてきたimageを削除するコード
+        unset($form['image']);
+        
+        //データベースに保存する
+        $news->fill($form);
+        $news->save();
+        
         return redirect('admin/news/create');
         
     }
--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -16,36 +16,30 @@
                    {{--count($errors)：$errorsはvalidateで弾かれた内容を記憶する配列
                    countメソッドは配列の個数を返すメソッド
                    エラーがなければ、$errorsはnullを返すのでcount($errors)は0を返す。--}}
-                    @if(count($errors)>0)
-                    <ul>
-                        {{--foreachは配列の数だけループする構文
-                        つまり$errorsの中身の数だけループをしてその中身を$eに代入している。--}}
-                        @foreach($errors->all() as $e)
-                        {{--$eに代入された中身を下記の文で表示している。
-                        Modelの設定が完了したらエラーメッセージがでるようになる--}}
-                        <li>{{ $e }}</li>
-                        @endforeach
-                    </ul>
+                    
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2">タイトル</label>
+                        <label class="col-md-2" for="title">タイトル</label>
                         <div class="col-md-10">
-                            {{--old('変数名')...入力フォームから送信など次画面に進んだ際、
-                            エラーがあって、最初の入力フォームに戻されたときに入力された内容を
-                            「自動で入れなおしてあげる」便利な機能--}}
                             <input type="text" class="form-control" name="title" value="{{ old('title') }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">本文</label>
+                        <label class="col-md-2" for="body">本文</label>
                         <div class="col-md-10">
                             <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
                         </div>
-                        <div class="form-group row">
-                            <label class="col-md-2">画像</label>
-                            <div class="col-md-10">
-                                <input type="file" class="form-control-file" name="image">
-                            </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">画像</label>
+                        <div class="col-md-10">
+                            <input type="file" class="form-control-file" name="image">
                         </div>
                     </div>
                     {{ csrf_field() }}
